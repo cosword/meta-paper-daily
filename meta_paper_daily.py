aa77ebe4f036711a8a132a817f7d4daf771ed617
cosword <48303708+cosword@users.noreply.github.com>
2022-11-03 18:12:43 +0800

    Update meta_paper_daily.py

--- meta_paper_daily.py
+++ meta_paper_daily.py
@@ -120,7 +120,7 @@ def json_to_md(data):
             f.write(f"## {keyword}\n\n")
             f.write("|Date|Title|Authors|PDF|Code|Comments|\n")
             # "|---|---|---|---|---|---|\n"
-            f.write("|:------|:---------------------|:-------|:-|:-|:---|\n")
+            f.write("|:------|:---------------------|:---|:-|:-|:---|\n")
             # sort papers by date
             # day_content = sort_papers(day_content)
 
