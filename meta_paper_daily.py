cb2ce8cead95751d6ad62bcd61c081cd3cd67df6
cosword <48303708+cosword@users.noreply.github.com>
2022-11-03 20:53:23 +0800

    Update meta_paper_daily.py

--- meta_paper_daily.py
+++ meta_paper_daily.py
@@ -11,7 +11,12 @@ papers = {}
 DateNow = datetime.date.today()
 DateNow = str(DateNow)
 DateNow = DateNow.replace('-', '.')
-per_key_papers = 10 # 只取10条
+per_key_papers = 10  # 只取10条
+
+import socks
+import socket
+socks.set_default_proxy(socks.SOCKS5, '127.0.0.1', int('1080'))
+socket.socket = socks.socksocket
 
 # 参考连接 https://zhuanlan.zhihu.com/p/425670267
 # 转换日期为标准格式 https://blog.csdn.net/weixin_43751840/article/details/89947528
@@ -22,11 +27,11 @@ def get_paper_from_arxiv(key):
     content = BeautifulSoup(res.text, 'html.parser')
     papers[key] = {}
     count = 1
-    
+
     # 正则匹配http
     pattern = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')  # 匹配模式
     for arxiv_result in content.find(name="ol").find_all(name='li'):
-        if count > per_key_papers:break
+        if count > per_key_papers: break
         paper_url = arxiv_result.find(attrs={'class': 'list-title'}).a['href']
         abstract = arxiv_result.find(class_="abstract-full")  # 只返回当前节点的text不包括子节点
         code_url = re.search(pattern, abstract.text)
@@ -36,8 +41,8 @@ def get_paper_from_arxiv(key):
         title = arxiv_result.find(class_='title')  # class是关键字所以要加下划线_
         title = title.text.strip()  # replace("<span class=\"search-hit mathjax\">Source</span>","").replace("</span>")text直接取出来了
         author = [au.strip() for au in arxiv_result.find(class_='authors').text.replace("\n", "").split(",")]
-        #author = ",".join(author)
-        author = author[0].replace("Authors:","") + "et.al" # 只取一作
+        # author = ",".join(author)
+        author = author[0].replace("Authors:", "") + "et.al"  # 只取一作
         submit_date = [item.text.replace("\n", "").replace("  ", "") for item in arxiv_result.select("p.is-size-7")]
         time_format = datetime.datetime.strptime(submit_date[0].split(";")[0], 'Submitted %d %B, %Y')
         format_date = f"{time_format.year}-{time_format.month}-{time_format.day}"
@@ -53,37 +58,39 @@ def get_paper_from_arxiv(key):
                 comments = "-"
         else:
             comments = "-"
-        #json_res.append(result)
-        #f.write("|Publish Date|Title|Authors|PDF|Code|Comments|\n" + "|---|---|---|---|\n")
+        # json_res.append(result)
+        # f.write("|Publish Date|Title|Authors|PDF|Code|Comments|\n" + "|---|---|---|---|\n")
         code_url = f"[code]({code_url})|" if code_url != "-" else "-|"
         if title not in papers:
             # 会议相关折叠
             comments = f"<details><summary>other</summary>{comments}</details>" if comments != "-" else "-"
             papers[key][title] = f"|**{format_date}**|**{title}**|{author}|[paper]({paper_url})|" + code_url + f"{comments}|\n"
+        count += 1
 
 
 def get_paper_from_google(key):
-    headers = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36 Edg/86.0.622.61',
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36 Edg/86.0.622.63',
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36 Edg/86.0.622.43',
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66',
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36',
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 Edg/88.0.705.63',
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36',
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36',
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36',
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.50',
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4346.0 Safari/537.36 Edg/89.0.731.0',]
+    headers = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36 Edg/86.0.622.61',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36 Edg/86.0.622.63',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36 Edg/86.0.622.43',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 Edg/88.0.705.63',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.50',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4346.0 Safari/537.36 Edg/89.0.731.0', ]
     query_key = key.replace(" ", "+")
     url = f"https://scholar.google.com/scholar?as_vis=0&q=allintitle:+{query_key}&hl=zh-CN&scisbd=1&as_sdt=0,5"
-    res = requests.get(url, headers={"User-Agent":random.choice(headers)})
+    res = requests.get(url, headers={"User-Agent": random.choice(headers)})
     content = BeautifulSoup(res.text, 'html.parser')
     body = content.find(id="gs_res_ccl_mid")
     # 谷歌学术爬虫
     now = datetime.datetime.today()
     count = 1
     for div in body.find_all(class_="gs_r gs_or gs_scl"):
-        if count > per_key_papers:break
+        if count > per_key_papers: break
         main = div.find(id=div.attrs['data-cid'])
         paper_url = main.attrs['href']
         title = main.text.strip()
@@ -113,8 +120,9 @@ def get_paper_from_google(key):
         format_date = f"{time_format.year}-{time_format.month}-{time_format.day}"
         if title not in papers:
             # 会议相关折叠
-            comment = f"<details><summary>comment</summary>{comment}</details>" if comments != "-" else "-"
+            comment = f"<details><summary>comment</summary>{comment}</details>" if comment != "-" else "-"
             papers[key][title] = f"|**{format_date}**|**{title}**|**{author}**|[paper]({paper_url})|" + code_url + f"{comment}|\n"
+        print(code_url)
         count += 1
 
 
@@ -141,7 +149,7 @@ def json_to_md(data):
             f.write(f"    <li><a href=#{kw}>{keyword}</a></li>\n")
         f.write("  </ol>\n")
         f.write("</details>\n\n")
-        #pass
+        # pass
 
     # write data into README.md
     with open(md_filename, "a+") as f:
@@ -162,9 +170,9 @@ def json_to_md(data):
 
             for _, v in day_content.items():
                 if v is not None:
+                    v = v.replace(u'\xa0', '')
                     f.write(v)
             f.write(f"\n")
-
 
 if __name__ == "__main__":
     for key in KEYS:
